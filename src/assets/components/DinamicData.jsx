b39f5576f9fd01b15924775999ce895e35956397
filgueira5 <dev.filgueiras@gmail.com>
2024-06-11 11:12:41 -0400

    Second Commit

---
+++ src/assets/components/DinamicData.jsx
@@ -0,0 +1,12 @@
+const DinamicData = () => {
+
+    const nome = prompt('Digite o seu nome :')
+
+    return (
+        <>
+            <p>O meu nome é {nome}</p>
+        </>
+    )
+}
+
+export default DinamicData;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import FirstComponent from './assets/components/FirstComponent'
+import DinamicData from './assets/components/DinamicData'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <FirstComponent/>
+    <DinamicData/>
   </React.StrictMode>,
 )
